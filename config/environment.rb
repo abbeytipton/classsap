d8e81076672bea51990016d23b2d412027834447
abbeytipton <abbeyktipton@gmail.com>
2019-04-20 17:00:10 -0500

    updating environment with stmp info for mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configuration for using SendGrid on Heroku
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app131145132@heroku.com",
+  :password => "a8l1kdlo7127",
+  :domain => "class-sap.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
